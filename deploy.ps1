b09c86f5c25490fd813645e93bc67784ad2639e3
James Ruskin <james.ruskin@questionmark.com>
2018-04-19 11:20:19 +0100

    Changes to remove QM references from the template
    Provides further options in the deploy.ps1 file, including:
    
    - Not requiring Questionmark.Configuration
    - Parameter for ShutdownNotificationEmail

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@
  .DESCRIPTION
     Deploys an Azure Resource Manager template
 #>
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName='NoOctopus')]
 param(
     # Purpose of the deployed template
     [string]$Purpose = 'QA',
@@ -15,35 +15,51 @@ param(
     [string]$SiteLocation = 'AUSE2',
 
     # Template to deploy
-    [ValidateScript( {Test-Path (Resolve-Path $_)})]
+    [ValidateScript({Test-Path (Resolve-Path $_)})]
     [string]$TemplateFilePath = "$PSScriptRoot\azuredeploy.json",
 
     # The URI to the OctopusDeploy server to connect to
-    [Parameter(ParameterSetName = 'OctopusDeploy', Mandatory)]
+    [Parameter(ParameterSetName = 'OctopusDeploy')]
+    [ValidateScript({(Invoke-RestMethod -Uri "$($_)/api").Application -eq 'Octopus Deploy'})]
     [string]$OctopusDeployApiUrl = "https://dev.deployment.questionmark.com",
 
     # If the VM should be connected to dev.deployment.questionmark.com, please provide a key here
     [Parameter(ParameterSetName = 'OctopusDeploy', Mandatory)]
     [string]$OctopusDeployApiKey,
 
-    [PSCredential]$LabCredential
+    # Target for the VM automated-shutdown notification e-mails
+    [string]$ShutdownNotificationEmail,
+
+    # Admin credential for the VM
+    [Parameter(Mandatory)]
+    [PSCredential]$LabCredential = 'ForgeQA'
 )
 $ErrorActionPreference = "Stop"
 
 $null = Set-AzureRmContext -SubscriptionName 'Forge Dev/Test'
 
-$ResourceGroupLocation = (Get-AzureRmLocation | Where DisplayName -eq (Get-QMAzureSiteLocation -SiteLocation $SiteLocation)[1]).Location
-$ResourceGroupName = Get-QMAzureResourceGroupName -Site $SiteLocation -DeploymentType FRG -Product $Purpose
+try {
+    $ResourceGroupName = Get-QMAzureResourceGroupName -Site $SiteLocation -DeploymentType FRG -Product $Purpose
+    $ResourceGroupLocation = (Get-AzureRmLocation | Where DisplayName -eq (Get-QMAzureSiteLocation -SiteLocation $SiteLocation)[1]).Location
+} catch [System.Management.Automation.CommandNotFoundException] {
+    # We assume that Questionmark.Configuration is not available
+    $ResourceGroupName = $SiteLocation + '-FRG' + $Purpose
+    $ResourceGroupLocation = switch ($SiteLocation) {
+        'AUSE2' {'East US 2'}
+        'AEUW1' {'West Europe'}
+        default {'West Europe'}
+    }
+}
 
 # Create or check for existing resource group
 if (-not ($ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue)) {
-    Write-Verbose "Creating resource group '$ResourceGroupName' in location '$ResourceGroupLocation'"
+    Write-Verbose "Creating resource group '$ResourceGroupName' in '$ResourceGroupLocation'"
     $null = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
 } else {
     Write-Verbose "Using existing resource group '$ResourceGroupName'"
 }
 
-# Start the deployment
+# Generate deployment parameters
 $DeploymentParameters = @{
     ResourceGroupName       = $ResourceGroupName
     TemplateFile            = (Resolve-Path $TemplateFilePath)
@@ -55,9 +71,13 @@ if ($PSBoundParameters.ContainsKey('OctopusDeployApiKey')) {
     $DeploymentParameters.TemplateParameterObject.OctopusDeployApiUrl = $OctopusDeployApiUrl
     $DeploymentParameters.TemplateParameterObject.OctopusDeployApiKey = $OctopusDeployApiKey
 }
+if ($PSBoundParameters.ContainsKey('ShutdownNotificationEmail')) {
+    $DeploymentParameters.TemplateParameterObject.autoShutdownNotificationEmail = $ShutdownNotificationEmail
+}
 if ($PSBoundParameters.ContainsKey('LabCredential')) {
     $DeploymentParameters.TemplateParameterObject.adminUsername = $LabCredential.Username
     $DeploymentParameters.TemplateParameterObject.adminPassword = $LabCredential.GetNetworkCredential().Password
 }
 
+# Start the deployment
 New-AzureRmResourceGroupDeployment @DeploymentParameters
